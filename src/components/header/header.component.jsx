7dc2ac03c4f1320f6d162109165d5e75c7eae246
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2021-06-24 01:03:39 +0530

    Adding the Cart Icon & Dropdown with Redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,9 +5,12 @@ import { auth } from "../../firebase/firebase.utils";
 
 import { ReactComponent as Logo } from "./../../assets/crown.svg";
 
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logoContainer" to="/">
       <Logo className="logo" />
@@ -28,12 +31,15 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden && <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
