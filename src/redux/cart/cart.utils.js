bc45472bdd04a55bbdf9bc471bd22b64ae28f29c
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2021-06-24 01:57:55 +0530

    Grouping Multiple Cart Item with Redux

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,14 @@
+export const addItemToCartUtil = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+  if (existingCartItem) {
+    return cartItems.map((item) =>
+      item.id === cartItemToAdd.id
+        ? { ...item, quantity: item.quantity + 1 }
+        : item
+    );
+  } else {
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  }
+};
