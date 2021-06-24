b50e48fceac34b6e3ad19e262c75167b9a529c53
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2021-06-24 17:55:41 +0530

    Re-modelling the Shop component through Collection Overview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,9 @@
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview";
-
-import { selectShopCollection } from "../../redux/shop/shop.selectors";
-
-const ShopPage = ({ collections }) => (
+const ShopPage = () => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionsOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollection,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
